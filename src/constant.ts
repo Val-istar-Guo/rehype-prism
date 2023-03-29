1bab898cd7ca809c514daf0ebb6869836aaffd03
val.istar.guo <val.istar.guo@gmail.com>
2023-03-29 17:50:11 +0800

    fix: don't import primjs toolbar and copy-to-clipboard plugins

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,4 @@
 import { RehypePrismPlugin } from './interface/rehype-prism-plugin.js'
 
 
-export const internalPlugins: RehypePrismPlugin[] = ['line-numbers']
+export const internalPlugins: RehypePrismPlugin[] = ['line-numbers', 'toolbar', 'copy-to-clipboard']
